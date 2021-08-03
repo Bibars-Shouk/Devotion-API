e37279e2c101cf66ea0a1fe195e17edf028c7097
Bibars-shouk <bibars.shouk1@gmail.com>
2021-08-03 18:28:31 +0300

    install mongoose and establish database connection

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 
-// Route files
-const bootcamps = require("./routes/bootcamps");
+const connectDB = require("./config/db");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
+
+// connect to database
+// *NOTE : We call connectDB after the dotenv config due to environment vars needed in the connectDB()
+connectDB();
+
+// Route files
+const bootcamps = require("./routes/bootcamps");
 
 const app = express();
 
@@ -19,6 +25,16 @@ app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server and exit process
+  server.close(() => {
+    // exit with failure
+    process.exit(1);
+  });
 });
