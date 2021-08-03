9ff12911b624488c7d7b57308c66d15eb27fddc1
Bibars-shouk <bibars.shouk1@gmail.com>
2021-08-04 00:09:25 +0300

    add and execute body parser on server js file update Bootcamp model create basic CRUD functionality on bootcamp controllers

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -44,11 +44,9 @@ const BootcampSchema = new Schema({
     type: {
       type: String,
       enum: ["Point"],
-      required: true,
     },
     coordinates: {
       type: [Number],
-      required: true,
       index: "2dsphere",
     },
     formattedAddress: {
