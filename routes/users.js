756febb188d533672f399f6c2135451b6baedbdd
Bibars-shouk <bibars.shouk1@gmail.com>
2021-08-13 00:01:28 +0300

    create a CRUD users only available to admins

---
+++ routes/users.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const router = express.Router({ mergeParams: true });
+
+const {
+  getUsers,
+  createUser,
+  getUser,
+  updateUser,
+  deleteUser,
+} = require("../controllers/users");
+const advancedResults = require("../middleware/advancedResults");
+const User = require("../models/User");
+const { protect, authorize } = require("../middleware/auth");
+
+router.use(protect);
+router.use(authorize("admin"));
+
+router.route("/").get(advancedResults(User), getUsers).post(createUser);
+router.route("/:id").get(getUser).put(updateUser).delete(deleteUser);
+
+module.exports = router;
