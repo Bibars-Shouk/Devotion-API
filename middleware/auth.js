b17de4adf4a9deb1ad296c1e00b4a55f961616ca
Bibars-shouk <bibars.shouk1@gmail.com>
2021-08-14 05:48:32 +0300

    use cookie check for user token and implement logout to clear the cookie

--- middleware/auth.js
+++ middleware/auth.js
@@ -10,13 +10,12 @@ exports.protect = asyncHandler(async (req, res, next) => {
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
   ) {
+    // set token from bearer token in header
     token = req.headers.authorization.split(" ")[1];
+  } else if (req.cookies.token) {
+    // set token from cookie
+    token = req.cookies.token;
   }
-
-  //  ? We are not gonna deal with the cookies for now
-  //   else if (req.cookies.token) {
-  //     token = req.cookies.token;
-  //   }
 
   // Make sure token exists
   if (!token) {
